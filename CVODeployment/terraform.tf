fcf1d8ac558419791ec9d94b9ec9f4ae3c48c93d
jorgeedugona <gjorge@netapp.com>
2021-05-10 13:04:23 +0100

    Update terraform.tf

--- CVODeployment/terraform.tf
+++ CVODeployment/terraform.tf
@@ -1,1 +1,55 @@
+variable "token" {
+  type = string
+}
 
+variable "location" {
+  type = string
+}
+
+variable "password" {
+  type = string
+}
+
+variable "client_id" {
+  type = string
+}
+
+variable "workspace_id" {
+  type = string
+}
+
+variable "license_single_node" {
+  type = string
+}
+
+variable "ha" {
+  type = bool
+}
+
+variable "subscription_id" {
+  type = string
+}
+
+variable "private_subnet_name" {
+  type = string
+}
+
+variable "vnet_name" {
+  type = string
+}
+
+variable "resource_group" {
+  type = string
+}
+
+variable "instance_type" {
+  type = string
+}
+
+variable "license_node_1" {
+  type = string
+}
+
+variable "license_node_2" {
+  type = string
+}
