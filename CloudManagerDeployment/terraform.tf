7d1218f2332e34a26c0aa6e5165e14aad72fbd1d
jorgeedugona <gjorge@netapp.com>
2021-05-10 12:58:39 +0100

    Update terraform.tf

--- CloudManagerDeployment/terraform.tf
+++ CloudManagerDeployment/terraform.tf
@@ -1,1 +1,44 @@
+variable "token" {
+  type = string
+}
 
+variable "location" {
+  type = string
+}
+
+variable "subscription_id" {
+  type = string
+}
+
+variable "company" {
+  type = string
+}
+
+variable "resource_group" {
+  type = string
+}
+
+variable "subnet_name" {
+  type = string
+}
+
+variable "vnet_name" {
+  type = string
+}
+
+variable "account_id" {
+  type = string
+}
+
+variable "admin_password" {
+  type = string
+}
+
+variable "admin_username" {
+  type = string
+}
+
+variable "publicIP" {
+  type = bool
+
+}
